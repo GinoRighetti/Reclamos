b853c5aa7062f66b55dd297be28aa31189a047ef
Gino Righetti <gino_righetti@hotmail.com>
2021-01-19 11:56:50 -0300

    Todo ok

---
+++ client/src/app/component/navegation-header/navegation-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navegation-header',
+  templateUrl: './navegation-header.component.html',
+  styleUrls: ['./navegation-header.component.css']
+})
+export class NavegationHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
