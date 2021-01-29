2a60d358bcf35f3b9cb85d917123bd4fe40c3ace
Gino <gino_righetti@hotmail.com>
2021-01-29 12:20:30 -0300

    Anda reclamos casi casi como se de debe

--- client/src/app/models/reclamos.ts
+++ client/src/app/models/reclamos.ts
@@ -1,10 +1,27 @@
 export interface Reclamo {
-    id?: number;
-    Fecha: Date;
+    N_reclamo?: number;
+    F_reclamo: Date;
     Contacto: string;
     Tj: string;
     Problema: string;
     Observaciones: string;
-    Usuario: string;
+    Id_u_r: number;
+    U_reclamo: string;
+    Estado: number;
+    N_solucion: number;
+    F_solucion: Date;
+    Solucion: string;
+    Derivacion: number;
+    Id_u_s: number;
+    U_solucion: string;
+}
+export interface ReclamoNuevo {
+    Id?: number;
+    Fecha: Date;
+    Contacto: string;
+    Id_tjs: string;
+    Problema: string;
+    Observaciones: string;
+    Id_Usuario: number;
     Estado: number;
 }
--- server/src/controlers/reclamosController.ts
+++ server/src/controlers/reclamosController.ts
@@ -5,13 +5,13 @@ import pool from '../routes/database';
 class ReclamosController{
 
     public async list (req: Request, res: Response) {
-        const reclamo = await pool.query("SELECT reclamos.Id, reclamos.Fecha, reclamos.Contacto, CONCAT(`Agencia`,'-',LPAD(`Subagencia`, 3, '0'),'-',`Maquina`) AS 'tj', reclamos.Problema, usuarios.Usuario, reclamos.Observaciones, reclamos.Estado FROM `reclamos`, `usuarios`, `tjs` WHERE ((reclamos.Id_tjs = tjs.Id) AND (reclamos.Id_Usuario = usuarios.Id))");
+        const reclamo = await pool.query("SELECT reclamos.Id as 'N_reclamo', reclamos.Fecha as 'F_reclamo', reclamos.Contacto, CONCAT(`Agencia`,'-',LPAD(`Subagencia`, 3, '0'),'-',`Maquina`) AS 'Tj', reclamos.Problema, reclamos.Observaciones, reclamos.Id_Usuario as 'id_u_r', usuarios.Usuario as 'U_reclamo', reclamos.Estado, soluciones.Id as 'N_solucion', soluciones.Fecha as 'F_solucion', soluciones.Solucion, soluciones.Derivacion, soluciones.Id_Usuario as 'Id_u_s', (SELECT usuarios.Usuario FROM usuarios WHERE usuarios.Id = soluciones.Id_Usuario) as 'U_solucion' FROM `reclamos` LEFT JOIN `usuarios` ON (reclamos.Id_Usuario = usuarios.Id) LEFT JOIN `tjs` ON (reclamos.Id_tjs=tjs.Id) LEFT JOIN `soluciones` ON (reclamos.Id=soluciones.Id_reclamo)");
         res.json(reclamo);
     }
 
     public async getOne (req: Request, res: Response) {
         const { id } = req.params;
-        const reclamo = await pool.query("SELECT reclamos.Id, reclamos.Fecha, reclamos.Contacto, CONCAT(`Agencia`,'-',LPAD(`Subagencia`, 3, '0'),'-',`Maquina`) AS 'tj', reclamos.Problema, usuarios.Usuario, reclamos.Observaciones, reclamos.Estado FROM `reclamos`, `usuarios`, `tjs` WHERE ((reclamos.Id_tjs = tjs.Id) AND (reclamos.Id_Usuario = usuarios.Id)) AND reclamos.Id = ?", [id]);
+        const reclamo = await pool.query("SELECT reclamos.Id as 'N_reclamo', reclamos.Fecha as 'F_reclamo', reclamos.Contacto, CONCAT(`Agencia`,'-',LPAD(`Subagencia`, 3, '0'),'-',`Maquina`) AS 'Tj', reclamos.Problema, reclamos.Observaciones, reclamos.Id_Usuario as 'id_u_r', usuarios.Usuario as 'U_reclamo', reclamos.Estado, soluciones.Id as 'N_solucion', soluciones.Fecha as 'F_solucion', soluciones.Solucion, soluciones.Derivacion, soluciones.Id_Usuario as 'Id_u_s', (SELECT usuarios.Usuario FROM usuarios WHERE usuarios.Id = soluciones.Id_Usuario) as 'U_solucion' FROM `reclamos` LEFT JOIN `usuarios` ON (reclamos.Id_Usuario = usuarios.Id) LEFT JOIN `tjs` ON (reclamos.Id_tjs=tjs.Id) LEFT JOIN `soluciones` ON (reclamos.Id=soluciones.Id_reclamo) WHERE reclamos.Id = ?", [id]);
         console.log(reclamo);
         if (reclamo.length > 0) {
             return res.json(reclamo[0]);
