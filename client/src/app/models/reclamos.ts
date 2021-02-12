5e9bbce3c0058307ee0152b01a20d855d9a55f78
Gino Righetti <gino_righetti@hotmail.com>
2021-02-12 09:59:05 -0300

    andando

--- client/src/app/models/reclamos.ts
+++ client/src/app/models/reclamos.ts
@@ -22,8 +22,17 @@ export interface ReclamoNuevo {
     Contacto: string;
     Metodo_Contacto: string;
     Id_tjs: string;
+    Motivo: string;
     Problema: string;
     Observaciones: string;
     Id_Usuario: number;
     Estado: number;
+}
+export interface Solucion {
+    Id?: number;
+    Id_reclamo: number;
+    Fecha: Date;
+    Solucion: string;
+    Id_usuario: number;
+    Derivacion: number;
 }
